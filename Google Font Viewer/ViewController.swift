c4a4853ab9ceb65d1ca12cb98fcc4173aaf2a169
Simon Lee <dev.simonlee@gmail.com>
2017-05-28 12:44:34 +0800

    Added Xcode project

---
+++ Google Font Viewer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Google Font Viewer
+//
+//  Created by Shao-Ping Lee on 5/28/17.
+//  Copyright © 2017 Simon Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
