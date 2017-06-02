5a62b0c028207df868ecabe75a24638c2da89e92
Simon Lee <dev.simonlee@gmail.com>
2017-06-02 15:49:20 +0800

    Cleaned up FontManager and added comments

--- Google Font Viewer/Font Management/FontManager.swift
+++ Google Font Viewer/Font Management/FontManager.swift
@@ -9,6 +9,8 @@
 import Foundation
 import PromiseKit
 
+typealias Cancel = () -> Void
+
 class FontManager {
     var postScriptNameMapping: [String: String]
     let queue = DispatchQueue(label: "font", qos: .background, attributes: [.concurrent])
@@ -17,37 +19,33 @@ class FontManager {
         self.postScriptNameMapping = postScriptNameMapping
     }
     
-    func font(for font: GoogleFont, size: CGFloat) -> (Promise<UIFont?>, () -> Void) {
+    /// Fetch an instance of UIFont for a font described by `GoogleFont`. 
+    /// A fresh font file will be downloaded and the font will be registered if the `FontManager` has not seen the font before.
+    ///
+    /// - Parameters:
+    ///   - font: `GoogleFont` describing the font
+    ///   - size: font size
+    /// - Returns: A pair of `Promise<UIFont?>` and `Cancel`. The cancel function is provided to allow caller to cancel the promise chain.
+    func font(for font: GoogleFont, size: CGFloat) -> (Promise<UIFont?>, Cancel) {
         if let postScriptName = postScriptNameMapping[font.name] {
             return (Promise(value: UIFont(name: postScriptName, size: size)), {})
         }
         
         return downloadAndRegisterFont(font: font, size: size)
-//
-//        let (promise, cancel) = downloadFont(font: font)
-//        
-//        return (promise.then { [weak self] data in
-//            self?.registerFont(font, data: data as CFData)
-//        }.then { postScriptName -> UIFont? in
-//            guard let postScriptName = postScriptName else { return nil }
-//            
-//            return UIFont(name: postScriptName, size: size)
-//        }, cancel)
-        
     }
-    
 }
 
 extension FontManager {
-    internal func fetchFontFromMapping(for font: GoogleFont, size: CGFloat) -> Promise<UIFont?> {
-        if let postScriptName = postScriptNameMapping[font.name] {
-            return Promise(value: UIFont(name: postScriptName, size: size))
-        } else {
-            return Promise(value: nil)
-        }
-    }
     
-    internal func downloadAndRegisterFont(font: GoogleFont, size: CGFloat) -> (Promise<UIFont?>, () -> Void) {
+    
+    /// Download font file from Google, register a CGFont,
+    /// then return a corresponding UIFont if possible.
+    ///
+    /// - Parameters:
+    ///   - font: describes the font
+    ///   - size: font size
+    /// - Returns: A pair of `Promise<UIFont?>` and `Cancel`. The cancel function is provided to allow caller to cancel the promise chain.
+    internal func downloadAndRegisterFont(font: GoogleFont, size: CGFloat) -> (Promise<UIFont?>, Cancel) {
         let (downloadPromise, cancel) = downloadFont(font: font)
         
         let downloadAndRegisterPromise = downloadPromise.then { [weak self] data in
@@ -61,7 +59,12 @@ extension FontManager {
         return (downloadAndRegisterPromise, cancel)
     }
 
-    internal func downloadFont(font: GoogleFont) -> (Promise<Data>, () -> Void) {
+    
+    /// Download font file from Google
+    ///
+    /// - Parameter font: `GoogleFont` that describes the font
+    /// - Returns: A pair of `Promise<Data>` and `Cancel` for further operations on `Data` or cancellation
+    internal func downloadFont(font: GoogleFont) -> (Promise<Data>, Cancel) {
         let request = Alamofire.request(font.externalDocumentURL)
         return (Promise { fulfill, reject in
             request.responseData(queue: queue) { response in
@@ -75,6 +78,13 @@ extension FontManager {
         }, request.cancel)
     }
     
+    
+    /// Register a CGFont for use in the app.
+    ///
+    /// - Parameters:
+    ///   - font: `GoogleFont` that describes the font
+    ///   - data: `CFData` representation of the font file
+    /// - Returns: `nil` if the registration fails, or the font's postscript name if successful
     internal func registerFont(_ font: GoogleFont, data: CFData) -> String? {
     
         let provider = CGDataProvider(data: data)
